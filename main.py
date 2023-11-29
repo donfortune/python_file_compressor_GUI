f8533cfee600ebf2f8764fd3c83f7b4ec670c621
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2023-11-29 20:06:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,6 @@ while True:
     filepaths = values['files'].split(';')
     folders = values['folders']
     create_zip(filepaths, folders)
-    window["output"].update(value="successfully compressed")
+    window["output"].update(value="compressed successfully ")
 
 window.close()
